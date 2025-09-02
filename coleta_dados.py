f53580605e470a3bc791e1d10a5b396f7e46aa4f
vini-haa <vinicius.andrade@somosicev.com>
2025-09-02 14:34:38 -0300

    E: Colocando o codigo em uma função para poder continuar com a analise no novo arquivo

--- coleta_dados.py
+++ coleta_dados.py
@@ -2,27 +2,38 @@ import requests # Faz requisições HTTP
 import urllib.parse # Manipula URLs
 import xml.etree.ElementTree as ET # Processa XML
 
-query = "Inteligência Artificial Piauí"
-query_formatada = urllib.parse.quote(query) # Formata a query para URL
+def coleta_dados(query = "Inteligência Artificial Piauí"):
+    
+    query_formatada = urllib.parse.quote(query) # Formata a query para URL
 
-url = f"https://news.google.com/rss/search?q={query_formatada}&hl=pt-BR&gl=BR&ceid=BR:pt-42"
-response = requests.get(url)
+    url = f"https://news.google.com/rss/search?q={query_formatada}&hl=pt-BR&gl=BR&ceid=BR:pt-42"
+    response = requests.get(url)
 
-print(url)
-print(f"Status Code: {response.status_code}")
+    arvore = ET.fromstring(response.content)
+    noticias = []
 
-arvore = ET.fromstring(response.content)
-noticias =[]
+    for item in arvore.findall('.//item'):
+        if len(noticias) >= 15: # limito a 15 notícias
+            break
 
-for item in arvore.findall('.//item'):
-    titulo = item.find('title').text
-    link = item.find('link').text
-    descricao = item.find('description').text
+        titulo = item.find('title').text
+        link = item.find('link').text
+        descricao = item.find('description').text
 
-    noticias.append({
-        'titulo': titulo,
-        'link': link,
-        'descricao': descricao
-    })
+        noticias.append({
+            'titulo': titulo,
+            'link': link,
+            'descricao': descricao
+        })
 
-print(len(noticias))
+
+    return noticias
+
+if __name__ == '__main__':
+    noticias_coletadas = coleta_dados()
+    
+    print(f"{len(noticias_coletadas)} notícias.")
+
+    if noticias_coletadas:
+        print("\n--- primeira notícia ---")
+        print(noticias_coletadas[0])
